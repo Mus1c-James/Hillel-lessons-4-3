5ab5d0e1b67c349c8c17a569e32e5fbb0b3a9172
Mus1c-James <ditmer2@gmail.com>
2024-12-12 06:29:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+  
+switch (true) {
+    case numOrStr === null:
+        console.log('ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
+}
+
+
+// const numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+
+// if (numOrStr === null) {
+//     console.log('ви скасували')
+// } else if (numOrStr.trim() === '') {
+//     console.log('Empty String');
+// } else if (isNaN(+numOrStr)) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
